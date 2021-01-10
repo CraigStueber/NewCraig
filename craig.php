df69fc7431508d8e00b52c972030629abb48d349
CraigStueber <craigstueber@gmail.com>
2021-01-10 11:51:09 -0500

    setting up new site

---
+++ craig.php
@@ -0,0 +1,21 @@
+<?php
+$name = $_POST['fname'];
+$lname = $_POST['lname'];
+$phone = $_POST['phonenumber'];
+$emailFrom = $_POST['email'];
+$message = $_POST['message'];
+
+$email_subject = 'New form submission';
+$email_body = "User First Name : $name \n" .
+    "User Last Name : $lname \n" .
+    "User Email : $emailFrom \n" .
+    "User Phone Number : $phone \n" .
+    "Message: $message \n";
+
+$to = "me@craigstueber.com";
+$headers = "From: " . $emailFrom;
+
+
+mail($to, $email_subject, $email_body, $headers);
+
+header("Location: https://craigstueber.com/portfolio.html?sent");
